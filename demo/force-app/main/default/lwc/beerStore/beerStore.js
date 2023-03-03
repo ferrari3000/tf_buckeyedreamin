b4286ab6625fe3b8ce010d143f211ed33b73c968
Trent Foor <trent.foor@cgi.com>
2023-03-02 22:24:35 -0800

    Styling and adding product photos

--- demo/force-app/main/default/lwc/beerStore/beerStore.js
+++ demo/force-app/main/default/lwc/beerStore/beerStore.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
 
-export default class Beer_Store extends LightningElement {}
+import lightningAppCss from '@salesforce/resourceUrl/lightningApp';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+
+export default class Beer_Store extends LightningElement {
+    renderedCallback() {
+        if(this.rendered) {
+            return;
+        }
+        this.rendered = true;
+
+        loadStyle(this, lightningAppCss)
+    }
+}
